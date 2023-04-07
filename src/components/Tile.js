73ff76565ae038b1c02e86605273f87d90df18a2
darq37 <dzmudziak@gmail.com>
2023-04-07 12:33:34 +0200

    Add colors for adjacent bombs

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,6 +1,27 @@
 import "./tile.css";
 
 const Tile = ({ tileState, checkBomb, setFlag, gameOver, onMouseDown }) => {
+  const getTileStyle = () => {
+	switch (tileState.adjBombs) {
+	  case 1:
+		return { color: "blue" };
+	  case 2:
+		return { color: "green" };
+		case 3:
+		return { color: "red" };
+		case 4:
+		return { color: "purple" };
+		case 5:
+		return { color: "black" };
+		case 6:
+		return { color: "gray" };
+		case 7:
+		return { color: "maroon" };
+	  default:
+		return { color: "black" };
+	}
+  }
+  
   return <div
 	onClick={ () => {
 	  checkBomb(tileState)
@@ -9,10 +30,12 @@ const Tile = ({ tileState, checkBomb, setFlag, gameOver, onMouseDown }) => {
 	  e.preventDefault();
 	  setFlag(tileState);
 	} }
-	onMouseDown={() => onMouseDown(true)}
-	onMouseOut={() => onMouseDown(false)}
-	onMouseUp={() => onMouseDown(false)}
+	onMouseDown={ () => onMouseDown(true) }
+	onMouseOut={ () => onMouseDown(false) }
+	onMouseUp={ () => onMouseDown(false) }
 	className="tile"
+	style={ getTileStyle() }
+	data-color={ tileState.adjBombs }
   >
 	{ tileState?.flag ? "F" : "" }
 	{ tileState?.question ? "?" : "" }
